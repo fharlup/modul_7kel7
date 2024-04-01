5d357cdce2f25fe882a9174c7bfe4859df5b1118
Jospeh <134275356+ItsRon23@users.noreply.github.com>
2024-04-01 08:00:28 +0700

    menambahkan class glossentry

--- Program.cs
+++ Program.cs
@@ -1,5 +1,8 @@
 ﻿var dataMahasiswa1302223083 = new DataMahasiswa1302223083();
 var datakelas1302223083 = new MemberData1302223083();
+var dataglosarium1302223083 = new GlossaryData1302223083();
 
- datakelas1302223083.ReadJSON();
+datakelas1302223083.ReadJSON();
 dataMahasiswa1302223083.ReadJSON();
+dataglosarium1302223083.ReadJSON();
+
