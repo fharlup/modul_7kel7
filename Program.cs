87474c0817637b0a6ccd1d473b194a479637a725
Jospeh <134275356+ItsRon23@users.noreply.github.com>
2024-04-01 07:46:57 +0700

    menambahkan class Member Data

--- Program.cs
+++ Program.cs
@@ -1,5 +1,5 @@
 ﻿var dataMahasiswa1302223083 = new DataMahasiswa1302223083();
-// var datakelas1302223083 = new KuliahMahasiswa1302223083();
+var datakelas1302223083 = new MemberData1302223083();
 
-// datakelas1302223083.ReadJSON();
+ datakelas1302223083.ReadJSON();
 dataMahasiswa1302223083.ReadJSON();
---
+++ no2_1302223083.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text.Json;
+
+public class MemberData1302223083
+{
+    public List<Member> members { get; set; }
+
+    public class Member
+    {
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+        public string gender { get; set; }
+        public int age { get; set; }
+        public string nim { get; set; }
+    }
+
+    public void ReadJSON()
+    {
+        string json = File.ReadAllText("jurnal7_2_1302223083.json");
+
+        var memberData = JsonSerializer.Deserialize<MemberData1302223083>(json);
+
+        if (memberData != null && memberData.members != null)
+        {
+            foreach (var member in memberData.members)
+            {
+                Console.WriteLine($"Name: {member.firstName} {member.lastName}");
+                Console.WriteLine($"Gender: {member.gender}");
+                Console.WriteLine($"Age: {member.age}");
+                Console.WriteLine($"NIM: {member.nim}");
+                Console.WriteLine();
+            }
+        }
+        else
+        {
+            Console.WriteLine("Member data tidak tersedia.");
+        }
+    }
+}
