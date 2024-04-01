159e3b31d51b4cc7d74f99d1b042006dbaddb3d3
Alvan <alvanmarvianto@gmail.com>
2024-04-01 07:57:43 +0700

    menambahkan class TeamMembers

---
+++ 1302223074/TeamMembers1302223074.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text.Json;
+
+public class MembersData1302223074
+{
+    public List<Member> members { get; set; }
+
+    public class Member
+    {
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+        public string gender { get; set; }
+        public int age { get; set; }
+        public string nim { get; set; }
+    }
+
+    public void ReadJSON()
+    {
+        string json = File.ReadAllText("D:\\Alvan\\kliah\\sem4\\kpl\\praktikum\\modul_7kel7\\1302223074\\jurnal7_2_1302223074.json");
+
+        var memberData = JsonSerializer.Deserialize<MembersData1302223074>(json);
+
+        if (memberData != null && memberData.members != null)
+        {
+            foreach (var member in memberData.members)
+            {
+                Console.WriteLine($"Name: {member.firstName} {member.lastName}");
+                Console.WriteLine($"Gender: {member.gender}");
+                Console.WriteLine($"Age: {member.age}");
+                Console.WriteLine($"NIM: {member.nim}");
+                Console.WriteLine();
+            }
+        }
+        else
+        {
+            Console.WriteLine("Member data tidak tersedia.");
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -2,3 +2,6 @@
 
 var dataMahasiswa1302223074 = new DataMahasiswa1302223074();
 dataMahasiswa1302223074.ReadJSON();
+
+var teamMembers1302223074 = new MembersData1302223074();
+teamMembers1302223074.ReadJSON();
