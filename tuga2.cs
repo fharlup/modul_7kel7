e609c6201fb530721c508317116395310861d830
fharlup <35958282+fharlup@users.noreply.github.com>
2024-04-01 07:49:24 +0700

    no 2

---
+++ tuga2.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text.Json;
+
+public class MemberData
+{
+    public List<Member> members { get; set; }
+
+    public class Member
+    {
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+        public string gender { get; set; }
+        public int age { get; set; }
+        public string nim { get; set; }
+    }
+
+    public void ReadJSON()
+    {
+        string json = File.ReadAllText("jurnal7_2_1302223032.json");
+
+        var memberData = JsonSerializer.Deserialize<MemberData>(json);
+
+        if (memberData != null && memberData.members != null)
+        {
+            foreach (var member in memberData.members)
+            {
+                Console.WriteLine($"Name: {member.firstName} {member.lastName}");
+                Console.WriteLine($"Gender: {member.gender}");
+                Console.WriteLine($"Age: {member.age}");
+                Console.WriteLine($"NIM: {member.nim}");
+                Console.WriteLine();
+            }
+        }
+        else
+        {
+            Console.WriteLine("Member data tidak tersedia.");
+        }
+    }
+}
