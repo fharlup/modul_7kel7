eebaab1269fbdeea7fe5f34a0d1c02fb66748db1
Jospeh <134275356+ItsRon23@users.noreply.github.com>
2024-04-01 07:42:54 +0700

    Menambahkan class datamahasiswa

--- Program.cs
+++ Program.cs
@@ -1,2 +1,5 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿var dataMahasiswa1302223083 = new DataMahasiswa1302223083();
+// var datakelas1302223083 = new KuliahMahasiswa1302223083();
+
+// datakelas1302223083.ReadJSON();
+dataMahasiswa1302223083.ReadJSON();
