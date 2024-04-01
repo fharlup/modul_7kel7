b152a5b23a9d5168b4b131bbe55d495b82ee663f
Yazid Al Ghozali <54970485+yazidalg@users.noreply.github.com>
2024-04-01 08:16:29 +0700

    Al Praktikan (#3)
    * add al
    
    * update json file
    
    * add files in program.cs

--- Program.cs
+++ Program.cs
@@ -1,9 +1,15 @@
-﻿var dataMahasiswa1302223083 = new DataMahasiswa1302223083();
-// var datakelas1302223083 = new KuliahMahasiswa1302223083();
+﻿//var dataMahasiswa1302223083 = new DataMahasiswa1302223083();
+//// var datakelas1302223083 = new KuliahMahasiswa1302223083();
+using System.Text.Json;
+
+//// datakelas1302223083.ReadJSON();
+//dataMahasiswa1302223083.ReadJSON();
+
+var dataMahasiswa1302223047 = new DataMahasiswa1302223047();
+dataMahasiswa1302223047.ReadJson();
 
 // datakelas1302223083.ReadJSON();
 dataMahasiswa1302223083.ReadJSON();
-using System.Text.Json;
 
 var dataMahasiswa1302223074 = new DataMahasiswa1302223074();
 dataMahasiswa1302223074.ReadJSON();
