d508857ab9fd451031bd5023dba6ca9a19527e16
Alvan <alvanmarvianto@gmail.com>
2024-04-01 08:03:30 +0700

    menambahkan class GlossaryData

--- Program.cs
+++ Program.cs
@@ -5,3 +5,6 @@ dataMahasiswa1302223074.ReadJSON();
 
 var teamMembers1302223074 = new MembersData1302223074();
 teamMembers1302223074.ReadJSON();
+
+var GlossaryData1302223074 = new GlossaryData();
+GlossaryData1302223074.ReadJSON();
